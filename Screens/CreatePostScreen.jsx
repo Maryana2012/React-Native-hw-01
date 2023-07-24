bc457cdd4c207c13d283f3426cb80ee7e2418857
Maryana2012 <maryana.oryshchuk@gmail.com>
2023-07-24 21:33:02 +0300

    camera and location

--- Screens/CreatePostScreen.jsx
+++ Screens/CreatePostScreen.jsx
@@ -1,4 +1,5 @@
-import { View, StyleSheet, Text, TouchableOpacity, TextInput, Image} from "react-native";
+import { View, StyleSheet, Text, TouchableOpacity, TextInput, Image, KeyboardAvoidingView, 
+         Platform, TouchableWithoutFeedback, Keyboard } from "react-native";
 import { Ionicons} from "@expo/vector-icons";
 import { useEffect, useState } from "react";
 import { Camera } from "expo-camera";
@@ -7,6 +8,7 @@ import * as Location from "expo-location";
 import * as MediaLibrary from "expo-media-library";
 
 export default function CreatePostScreen() {
+     const [keyboardOpen, setKeyboardOpen] = useState(false);
     const [hasPermission, setHasPermission] = useState(null);
     const [cameraRef, setCameraRef] = useState(null);
     const [type, setType] = useState(Camera.Constants.Type.back);
@@ -40,15 +42,20 @@ export default function CreatePostScreen() {
          setLocation(coords);
         })();
     }, []);
+
     
     if (hasPermission === null) {return <View /> }
     if (hasPermission === false) { return <Text>No access to camera</Text> }
+    const keyboardDidShow = () => {setKeyboardOpen(true); };
+    const keyboardDidHide = () => { setKeyboardOpen(false); };
+    Keyboard.addListener("keyboardDidShow", keyboardDidShow);
+    Keyboard.addListener("keyboardDidHide", keyboardDidHide);
 
     const data = photo && photoName && locationName;
 
     const handleMakePhoto = async ()=>{
-       if (cameraRef) {
-         const { uri } = await cameraRef.takePictureAsync();
+        if (cameraRef) {
+        const { uri } = await cameraRef.takePictureAsync();
         await MediaLibrary.createAssetAsync(uri);
         setPhoto(uri)
         } 
@@ -57,16 +64,15 @@ export default function CreatePostScreen() {
     const handleCreatePublish = () => {
         if (data) {
             navigation.navigate("Post",
-            {location: location,
+               {location: location,
                 photo: photo,
                 photoName: photoName,
-                locationName: locationName});
-            setPhotoName("");
-            setLocationName("");
-            setPhoto("");
-            // console.log("photo: ", photo);
-            // console.log("PhotoName: ", photoName);
-            // console.log("LocationName: ", locationName);
+                locationName: locationName
+                });
+            
+            setLocationName(null);
+            setPhotoName(null);
+            setPhoto(null);
         } else {
             console.log("Please, fill all fields");
             return;
@@ -77,13 +83,16 @@ export default function CreatePostScreen() {
         setLocationName(null);
         setPhoto(null);
         setPhotoName(null);
-        // console.log("PhotoName: ", photoName);
-        // console.log("LocationName: ", locationName);
     }
     
-    console.log("photo: ", photo);
     return (
         <View style={styles.container}>
+            
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}> 
+            <KeyboardAvoidingView  
+             behavior={Platform.OS == "ios" ? "padding" : "height"}
+             keyboardVerticalOffset={Platform.OS == "ios" ? 0 : 0}
+             style={styles.container}>  
             <View style={styles.header}>
               <TouchableOpacity onPress={() => { navigation.navigate("Post") }} >
                  <Ionicons name="arrow-back-outline" size={24} color="#000"/>
@@ -94,15 +103,20 @@ export default function CreatePostScreen() {
 
             <View style={styles.mainContent}>
 
-                <Camera style={styles.main_photo}
-                 type={type}
-                 ref={setCameraRef}>
-                    {photo && <Image source={{ uri: photo }} style={styles.imageDone} />}
-                    <TouchableOpacity style={styles.photoButton} onPress={handleMakePhoto}>
-                        <Ionicons name="camera-outline" size={24} color="#000" style={styles.photoIcon}/>
-                    </TouchableOpacity>   
-                </Camera>
-                            
+                <Camera type={type} ref={setCameraRef} style={styles.cameraContainer}>
+                    <View style={styles.photoView}>
+                        {photo && (
+                            <View style={styles.photoContainer}>
+                                
+                                <Image source={{ uri: photo }} style={{ width: "100%", height: "100%" }} />
+                            </View>)}
+                      <TouchableOpacity style={styles.photoButton} onPress={handleMakePhoto}>
+                         <Ionicons name="camera-outline" size={24} color="#000" style={styles.photoIcon}/>
+                      </TouchableOpacity>   
+                    </View>
+                  </Camera>
+
+                                       
                 <Text style={styles.text}>
                     {photo ? "Редагувати фото" : "Завантажте фото"}
                 </Text>
@@ -119,31 +133,34 @@ export default function CreatePostScreen() {
                     style={styles.inputLocation}
                     value={locationName}
                     onChangeText={(text) => setLocationName(text)} />
-             
-                <TouchableOpacity
-                    // style={styles.button}
-                      style={[styles.button, !data && styles.buttonDisabled]}
+                
+                {!keyboardOpen && (<>           
+                <TouchableOpacity style={[styles.button, !data && styles.buttonDisabled]}
                     onPress={handleCreatePublish}>
                     <Text style={styles.buttonText}>Опублікувати</Text>
                 </TouchableOpacity>
+                        </>)}
             </View>     
 
-
+            {!keyboardOpen && (<>
             <View style={styles.footer}>
-               
                 <TouchableOpacity style={styles.buttonDelete} onPress={handleClearFields}>
                    <Ionicons name="trash-outline" size={24} color="#000"/>
                 </TouchableOpacity>
             </View>
-
             <View style={styles.divEnd}></View>
-           
+            </>
+           )}
+
+             </KeyboardAvoidingView>  
+            </TouchableWithoutFeedback>  
         </View>
     )
     }
     
 const styles = StyleSheet.create({
     container: {
+        flex:1,
         position: "absolute",
         bottom: 0,
         right: 0,
@@ -151,8 +168,6 @@ const styles = StyleSheet.create({
         height: '100%',
         alignItems: "center",
         justifyContent: "space-around",
-        borderBottomLeftRadius: 0,
-        borderBottomRightRadius: 0,
         backgroundColor: 'blue'
     },
     header: {
@@ -175,18 +190,11 @@ const styles = StyleSheet.create({
     },
     
     mainContent: {
+        flex: 1,
         width: "100%",
         height:550,  
     },
-    main_photo:{
-        position:"relative",
-        width:343,
-        height:240,
-        backgroundColor: "orange",
-        marginLeft:25,
-        borderRadius:10,
-        marginTop:20,
-    },
+   
     photoButton:{
         position:'absolute',
         width: 40,
@@ -195,19 +203,13 @@ const styles = StyleSheet.create({
         borderRadius: 50,
         top: 100,
         left: 150,
-        zIndex:12
+        zIndex:20
     },
     photoIcon:{
       position:"absolute",
       top: 7,
       left:8,
       zIndex:10
-    },
-    imageDone: {
-        position:"absolute",
-        width: "100%",
-        height: "100%",
-        // zIndex:15
     },
     text:{
       fontSize:16,
@@ -269,16 +271,17 @@ const styles = StyleSheet.create({
         fontSize: 16,
     },
     footer: {
-      width: '100%',
+       
+        width: '100%',
         height: 60,
         padding: 11,
-      
         flexDirection: "row",
         alignItems: "center",
         justifyContent: "space-evenly"
     },
 
     buttonDelete: {
+       
         width: 70,
         height: 40,
         backgroundColor: "#FF6C00",
@@ -291,5 +294,21 @@ const styles = StyleSheet.create({
         height: 5,
         backgroundColor: '#212121',
         marginBottom: 8,
-    }
+    },
+    cameraContainer: {
+        position: "relative",
+        width:343,
+        height:240,
+        marginLeft:25,
+        borderRadius:20,
+        marginTop:20,
+    },
+    photoView: {
+        flex: 1,
+        borderRadius:20,
+        backgroundColor: "transparent",
+        justifyContent: "flex-end",
+    },
+
+
 })
